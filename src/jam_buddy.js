e0ca9b3fd4fa42c5ea2b4d1726a61c7d8ee360eb
Mnqanqeni <mnqanqenindiyakholwa206@gmail.com>
2024-01-31 21:23:25 +0200

    initial commit

--- src/jam_buddy.js
+++ src/jam_buddy.js
@@ -3,11 +3,13 @@ class JamBuddy {
     currentNotes = [];
 
     randomizeCurrentNotes() {
-        for (let i = JamBuddy.musicalElements.length - 1; i > 0; i--) {
+        let shuffleList= ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+        for (let i = shuffleList.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [JamBuddy.musicalElements[i], JamBuddy.musicalElements[j]] = [JamBuddy.musicalElements[j], JamBuddy.musicalElements[i]];
+            [shuffleList[i], shuffleList[j]] = [shuffleList[j], shuffleList[i]];
         }
-        this.currentNotes = [JamBuddy.musicalElements[0], JamBuddy.musicalElements[1]];
+        this.currentNotes = [shuffleList[0], shuffleList[1]];
+
         return this.currentNotes;
     }
 
@@ -15,16 +17,18 @@ class JamBuddy {
         return this.currentNotes;
     }
 
-    checkAnswer(element1, element2) {
-        const index1 = JamBuddy.musicalElements.indexOf(element1);
-        const index2 = JamBuddy.musicalElements.indexOf(element2);
+    checkAnswer(distance) {
+        const index1 = JamBuddy.musicalElements.indexOf(this.getCurrentNotes()[0]);
+        const index2 = JamBuddy.musicalElements.indexOf(this.getCurrentNotes()[1]);
 
         if (index1 === -1 || index2 === -1) {
             return null;
         }
+        console.log(index1)
+        console.log(index2)
         const absDiff = Math.abs(index1 - index2);
         const cyclicDistance = [absDiff, JamBuddy.musicalElements.length - absDiff];
-
+        console.log(cyclicDistance)
         return cyclicDistance.includes(distance);
     }
 
@@ -36,3 +40,17 @@ class JamBuddy {
         }
     }
 }
+
+buddy = new JamBuddy()
+console.log(buddy.randomizeCurrentNotes())
+console.log(buddy.getCurrentNotes()) // let's say this returns ['C','D#']
+console.log(buddy.checkAnswer(1)) // This will return a boolean False
+console.log(buddy.checkAnswer(2)) // False again
+console.log(buddy.checkAnswer(3)) // This is correct, so it returns True
+console.log(buddy.checkAnswer(9)) // This is also correct => True
+
+console.log(buddy.setCurrentNotes(['A','A#']))
+console.log(buddy.getCurrentNotes()) // this will return ['A','A#']
+console.log(buddy.checkAnswer(1))
+console.log(buddy.checkAnswer(11)) // returns a boolean True
+//["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
